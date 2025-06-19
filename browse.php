188028253c777ae824e44a355029d121fad95232
koinuko <koinuko@yahoo.com>
2025-06-18 21:16:29 -0700

    Form adds post to database
    item.php and browse.php had their img directories adjusted to account for upload adding entire directory to DB. perhaps later change them to a variable $dir but it works fine for now

---
+++ browse.php
@@ -0,0 +1,151 @@
+<?php 
+require 'config.php';
+?>
+
+<html>
+  
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Browse</title>
+
+<style>
+
+	.card {
+	width:200px;
+	height:250px;
+	border: 1px solid black;
+	border-radius:3px;
+    background:white;
+}
+
+	.cardtitle {
+	text-align:center;
+	height:36px;
+	line-height:36px;
+	margin:0px;
+}
+
+	.cardinfo {
+	height:32px;
+	line-height:32px;
+	margin:0px
+}
+  
+    .cardimg {
+    width:150px; 
+    height:150px; 
+    margin:auto;
+    text-align:center
+    }
+
+	.toitem {
+	height:32px;
+	line-height:32px;
+	margin:0px
+
+}
+
+</style>
+
+</head>
+
+<body>
+  <div id="header" style="width:auto; border-bottom:1px solid black; margin:10px">
+  	<p>header stuff</p>
+  </div> 
+  
+  <div id="main" style="margin:auto; max-width:80%; display:flex; flex-direction:row; flex-wrap:wrap; gap:10px%; justify-content:space-around">
+  	<div id="left" style="width:25%; min-width:200px; background-color:#0000FF50; height:500px">
+  	</div>
+  	
+  	
+  	<div id="right" style="width:70%; min-width:500px; background-color:#00FF0050; height:500px; display:flex; flex-direction:row; flex-wrap:wrap">
+      
+      <?php
+      
+      /* Attempt to connect to MySQL database */
+      $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+      // Check connection
+      if($link === false){
+          die("ERROR: Could not connect. " . mysqli_connect_error());
+      } 
+      
+       // find out how many rows are in the table 
+      $sql = "SELECT COUNT(*) FROM post";
+      $result = $link->query($sql) or trigger_error("SQL", E_USER_ERROR);
+      $r = mysqli_fetch_row($result);
+      $numrows = $r[0];
+      
+// PAGINATION 
+     
+      // number of rows to show per page
+      $rowsperpage = 20;
+      // find out total pages
+      $totalpages = ceil($numrows / $rowsperpage); 
+      // echo $totalpages; ;; TEST
+
+      // get the current page or set a default
+      if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+         // cast var as int
+         $currentpage = (int) $_GET['currentpage'];
+      } else {
+         // default page num
+         $currentpage = 1;
+      } // end if
+
+      // if current page is greater than total pages...
+      if ($currentpage > $totalpages) {
+         // set current page to last page
+         $currentpage = $totalpages;
+      } // end if
+      // if current page is less than first page...
+      if ($currentpage < 1) {
+         // set current page to first page
+         $currentpage = 1;
+      } // end if
+
+      // the offset of the list, based on current page 
+      $offset = ($currentpage - 1) * $rowsperpage;
+      
+// END PAGINATION
+      
+// BUILD ITEM LIST
+      
+      $sql = "SELECT * FROM post ORDER BY id DESC LIMIT $offset, $rowsperpage;";
+		$result = $link->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+        
+  echo '<div class="card">';
+  echo '<p class="cardtitle">'.$row["name"].'</p>';
+  echo '<div class="cardimg" style="">';
+  echo '<img src="'.$row["thumb"].'" style="max-width:150px; max-height:150px;"/>
+  			</div>
+  			<p class="cardinfo">Info</p>
+  			<p class="toitem">
+  				<a href="item.php?id='.$row["id"].'">View Item</a>
+  			</p>';
+  echo '</div>';
+
+  }
+}
+      
+/******  build the pagination links ******/
+// COPY THIS INFO FROM WEBRING MEM'S LIST WHEN OVER 20 ITEMS ADDED
+    
+    $link->close();
+    
+    ?> 
+      
+  	</div>
+  	
+  </div>
+  
+</body>
+  
+  
+  
+</html>
---
+++ upload.php
@@ -0,0 +1,357 @@
+<?php
+	
+	// config
+	require 'config.php';
+    
+	// show errors
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);  
+
+
+
+
+
+
+
+	// Uploads images; checks for completion; shows success or error messages
+    $uploaded_filenames = '';
+	// if file has been selected:
+	// > require an image. require the text boxes be filled out.
+	// allow image > check text boxes > move image > move txt to db > create thumbnail
+	if ($_FILES != null) {
+      if ($_FILES['files']['name'] != null) { 
+
+        // upload image to folder 
+        
+        	// select directory for fullsize img
+            $target_dir = "items/";
+        	$filenoext = explode('.', $_FILES['files']['name']);
+            $target_file = $target_dir . basename($_FILES['files']['name']);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        
+            // Check if image file is a actual image or fake image             
+            if(isset($_POST["submit"])) {
+              $check = getimagesize($_FILES['files']['name']);
+              if($check !== false) {
+                echo "File is an image.";
+                $uploadOk = 1;
+              } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+              }
+            }           
+        
+        	// Check if file already exists
+            if (file_exists($target_file)) {
+              echo "Sorry, file already exists.";
+              $uploadOk = 0;
+             }
+        
+        	// Check file size; current max 500KB (500 x 1000 = 500000)
+            if ($_FILES['files']["size"] > 500000) {
+              echo "Sorry, your file is too large.";
+              $uploadOk = 0;
+            }
+        
+        	// Allow certain file formats: only jpg, png, gif
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+              echo "Sorry, only JPG, JPEG, PNG, & GIF files are allowed.";
+              $uploadOk = 0;
+            }
+        
+        	// Check if $uploadOk is set to 0 by an error
+            if ($uploadOk != 1) {
+              echo "Sorry, your file was not uploaded due to a file error.";
+            // if everything is ok, try to upload file
+            } else {             
+              
+         	// IF selected image is ready to be uploaded:             
+            // actual upload of file
+              if (move_uploaded_file($_FILES['files']['tmp_name'], $target_file) != false) {               
+                
+                // create thumb after successful upload
+                           
+                /**
+                * Resize an image and keep the proportions
+                * @author Allison Beckwith <allison@planetargon.com>
+                * @param string $filename
+                * @param integer $max_width
+                * @param integer $max_height
+                * @return image
+                */
+                
+                function resizeImage($filename, $max_width, $max_height)
+                {
+                    list($orig_width, $orig_height) = getimagesize($filename);
+
+                    $width = $orig_width;
+                    $height = $orig_height;
+
+                    /*# taller
+                    if ($height > $max_height) {
+                        $width = ($max_height / $height) * $width;
+                        $height = $max_height;
+                    }
+
+                    # wider
+                    if ($width > $max_width) {
+                        $height = ($max_width / $width) * $height;
+                        $width = $max_width;
+                    }
+                    */
+                  
+                    $image_p = imagecreatetruecolor( intval($width), intval($height));                                    
+
+                    switch(mime_content_type($filename)) {
+                      case 'image/png':
+                        $img = imagecreatefrompng($filename);
+                        break;
+                      case 'image/gif':
+                        $img = imagecreatefromgif($filename);
+                        break;
+                      case 'image/jpeg':
+                        $img = imagecreatefromjpeg($filename);
+                        break;
+                      case 'image/bmp':
+                        $img = imagecreatefrombmp($filename);
+                        break;
+                      default:
+                        $img = null; 
+                      }
+                      return $img;                
+
+                    imagecopyresampled($image_p, $img, 0, 0, 0, 0, 
+                                                     $width, $height, $orig_width, $orig_height);
+                    return $image_p;
+                }
+                
+                // uploading thumb as JPEG in the thumb folder
+                $thumb = resizeImage($target_file, 150, 150);
+                $thumb = imagescale($thumb, 150);
+                $thumbdir = 'items/thumb/';
+                $target_thumb = $thumbdir . $filenoext[0] . '.jpg';
+                imagejpeg($thumb, $target_thumb);  
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                // define variables for form and set to empty values
+                // isset check for each variable to prevent error
+                $file = $title = $notes = $tags = "";
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                  /* removing bc image is handled below; remove if everything works ok
+                  if (isset($_POST["files"])) {
+                    $file = test_input($_POST["files[]"]);
+                  }
+                  */
+                  if (isset($_POST["posttitle"])) {
+                    $title = test_input($_POST["posttitle"]);
+                  }
+                  if (isset($_POST["postnotes"])) {
+                    $notes = test_input($_POST["postnotes"]);
+                  } else {
+                    $notes = '';
+                  }
+                  if (isset($_POST["posttags"])) {
+                    $tags = test_input($_POST["posttags"]);
+                  } else {
+                    $tags = '';
+                  }
+                }
+
+                // function to clean submitted data IF data is defined
+                function test_input($data) {
+                  if ($data != null) {
+                    $data = trim($data);
+                    $data = stripslashes($data);
+                    $data = htmlspecialchars($data);
+                    // remove this line below if run into issues w data upload
+                    $data = mysqli_real_escape_string($data);
+                    return $data;
+                  }
+                }
+                
+                
+                
+                
+                
+                // SQL for adding form info to database. Only the title and image are required at first upload.
+                // ability to edit description, tags, etc. will come later, in an edit function; right now
+                // focus on getting the new post to show up in the browse page, with the title and thumbnail.
+
+                
+                
+                
+                
+                if (isset($title)) {
+                  
+                  $sql = $link->prepare('INSERT INTO `post` (`name`, `img`, `thumb`, `description`)
+                  VALUES (?, ?, ?, ?)');
+                  $sql->bind_param("ssss", $title, $target_file, $target_thumb, $notes);
+                  $sql->execute();
+                  
+                  
+                }
+
+                // success or error message
+             
+                if ($sql->execute() === TRUE) {
+                	$uploaded_filenames = 'Successfully uploaded; view your new post here [link].<br/><br/>';
+                } else {
+                	echo "Error adding information to database";
+                }
+                
+                
+                
+              } else {
+                echo "Sorry, there was an error saving your file.";
+              }
+            }
+       
+      } else {
+      }
+ 	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Upload Mockup</title>
+    
+    <style>
+        input {
+            border:2px solid #CCC;
+            background-color:#fafafa;
+            width:90%;
+            padding:8px;
+            border-radius:15px;
+        }
+      
+      input[type="text"] {
+      height:120px
+      }
+      
+      input[type="submit"] {
+      padding:5px;
+      border:2px solid black;
+      cursor:pointer
+      }
+      
+    </style>
+    
+</head>
+
+<body>   
+  
+      <?php echo $uploaded_filenames; ?>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data">
+      <div id="uploadMain" style="display:flex; flex-direction:row; margin:auto; text-align:center; justify-content:space-around; width:90%; flex-wrap:wrap">
+        <div id="uploadLeft" style="width:33%">
+          <div id="uploadPreview" style="width:90%; border-radius:25px; background-color:#CCC; padding:10px; min-height:500px; min-width:250px; display:flex; flex-direction:column; justify-content:space-around">              
+            <div class="box">
+                <label>
+                  <strong>*Choose a file</strong>
+                  <span>or drag it here</span>
+                  <br/>
+                  <br/>
+                  <img id="preview" alt="Preview the file you're uploading." src="glyphs/addphoto.png" style="max-width:200px; max-height:200px" />
+                  <br/>
+                  <br/>
+                  <input onchange="loadFile(event)" id="fileInp" class="box__file" type="file" name="files" accept="image/*" />
+                </label>
+                <div class="file-list"></div>
+                <br/>
+                <div id="mult"></div>
+                <br/>
+              </div>
+              <input type="submit">
+          </div>
+        </div>       
+        
+        <div id="uploadRight" style="width:66%; min-width:500px">
+          <p>
+            *Title
+          </p>
+          <input type="text" id="posttitle">
+
+          <p>
+            Notes
+          </p>
+          <input type="text" id="postnotes">
+
+          <p>
+            Tags (drop down? comma separated?)
+          </p>
+          <input type="text" id="posttags">
+
+        </div>        
+        
+      </div>  
+      </form>
+                
+<!-- SCRIPT ------------------------------------------------------------------------------------------ -->
+  
+    <script>
+
+        const box = document.querySelector('.box');
+        const fileInput = document.querySelector('[name="files"');
+        const selectButton = document.querySelector('label strong');
+        const fileList = document.querySelector('.file-list');
+
+        let droppedFiles = [];
+
+      	// draggable
+        [ 'drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop' ].forEach( event => box.addEventListener(event, function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        }), false );
+
+        [ 'dragover', 'dragenter' ].forEach( event => box.addEventListener(event, function(e) {
+            box.classList.add('is-dragover');
+        }), false );
+
+        [ 'dragleave', 'dragend', 'drop' ].forEach( event => box.addEventListener(event, function(e) {
+            box.classList.remove('is-dragover');
+        }), false );
+
+        box.addEventListener('drop', function(e) {
+            droppedFiles = e.dataTransfer.files;
+            fileInput.files = droppedFiles;
+            updateFileList();
+        }, false );
+
+        fileInput.addEventListener( 'change', updateFileList );
+        fileInput.addEventListener( 'change', loadFile );
+		
+      
+      	// previews selected file for user + warn for mult files
+        function updateFileList() {
+            const filesArray = Array.from(fileInput.files);
+            if (filesArray.length > 1) {
+                fileList.innerHTML = `<p>Selected file: ${filesArray[0].name}</p>`;
+                document.getElementById('preview').src = window.URL.createObjectURL(filesArray[0]);
+                document.getElementById('mult').innerHTML = "If you have selected multiple files, only the image previewed above will be uploaded.";
+            } else if (filesArray.length == 1) {
+                fileList.innerHTML = `<p>Selected file: ${filesArray[0].name}</p>`;
+                document.getElementById('preview').src = window.URL.createObjectURL(filesArray[0]);
+            } else {
+                fileList.innerHTML = '';
+            }
+        }
+      
+    </script>
+    
+</body>
+</html>
